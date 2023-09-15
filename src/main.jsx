d4b88ba5b7f8c3ceafdb6079f26ca8d6e80f7d17
LuisMZampar <rm550531@fiap.com.br>
2023-09-15 16:26:29 -0300

    Atualizando o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 //BLOCO DE ROTAS
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import EditarProdutos from './routes/EditarProdutos.jsx';
-import Home from './routes/Home.jsx';
-import Produtos from './routes/Produtos.jsx';
-import Erro404 from './routes/Erro404.jsx';
-import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+import Produtos from "./routes/Produtos.jsx";
+import Erro404 from "./routes/Erro404.jsx";
+import ExcluirProdutos from "./routes/ExcluirProdutos.jsx";
+import EditarProdutos from "./routes/EditarProdutos.jsx";
+import AdicionarProdutos from "./routes/AdicionarProdutos.jsx";
 
 const router = createBrowserRouter([
-  { path: '/' , element: <App/>,
-    errorElement: <Erro404/>,
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Erro404 />,
     children: [
-      { path: '/' , element: <Home/>},
-      { path: '/produtos' , element: <Produtos/>},
-      { path: '/editar/produtos/:id' , element: <EditarProdutos/>},
-      { path: '/excluir/produtos/:id' , element: <ExcluirProdutos/>},
-    ]
-  }
+      { path: "/", element: <Home /> },
+      { path: "/produtos", element: <Produtos /> },
+      { path: "/editar/produtos/:id", element: <EditarProdutos /> },
+      { path: "/excluir/produtos/:id", element: <ExcluirProdutos /> },
+      { path: "/adicionar/produtos", element: <AdicionarProdutos /> },
+    ],
+  },
 ]);
 //BLOCO DE ROTAS
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <RouterProvider router={router}/>
-  ,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
