41490916c0ae32daf3c518524c04113333e94f45
Gabriel-Galdino22 <rm50711@fiap.com.br>
2023-09-15 21:41:24 -0300

    Alterando css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Rodape from "./components/Rodape";
 import Lista from "./components/Lista";
 import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
@@ -12,7 +11,6 @@ export default function App() {
         {/* Criar um elemento de conteudo com um section, uma div, dois parágrafos, cada um com 3 linhas de lorem e uma imagem. */}
         <Outlet />
         {/* Criar um rodapé com uma div, uma lista ul e 3 elementos li com itens de link para redes sociais e um parágrafos com o seguinte texto : "Todos os meus direitos reservados - 2023", não esqueça de colocar o código do símbolo de copyright. */}
-        <Rodape />
       </div>
     </>
   );
